a11c710673ead9d4bd302c693f36d39bcfe84f20
Shaikh Adeeb <shaikhadeeb.work@outlook.com>
2026-06-02 15:47:15 +0100

    Format Terraform configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
 
-backend "azurerm" {
-  resource_group_name  = "rg-tfstate-ai-infra-uksouth"
-  storage_account_name = "sttfstateaiinfraadeeb"
-  container_name       = "tfstate"
-  key                  = "dev.terraform.tfstate"
-}
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-ai-infra-uksouth"
+    storage_account_name = "sttfstateaiinfraadeeb"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
